4e8b56d16adcffdf606c8e49a22e768245ae68bb
Chuck Nelson <chachimcdev@gmail.com>
2021-05-28 14:06:41 -0700

    setup configs for firebase db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTOCTP5xB3-MswVNguQ5Gn9RnR3ouGH5U",
-  authDomain: "expense-app-basic.firebaseapp.com",
-  databaseURL: "https://expense-app-basic-default-rtdb.firebaseio.com",
-  projectId: "expense-app-basic",
-  storageBucket: "expense-app-basic.appspot.com",
-  messagingSenderId: "682131878717",
-  appId: "1:682131878717:web:84bd775870883e9ce64b54",
-  measurementId: "G-DX2PNR8CYX"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,3 +18,4 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 export { firebase, database as default };
+
