1e74c4f0354507ff4e5e899253b8d84e4a89502e
Chuck Nelson <chachimcdev@gmail.com>
2021-06-12 11:06:42 -0700

    styles for login

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LogInPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expense Tracker App</h1>
+            <p>Do you know where your expenses are right now?</p>
+            <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>      
     </div>
 );
 
